3ed5fb814175b32f179549826dedbdb144d0f082
Puiluiz <Bearmuzz@gmail.com>
2023-08-01 20:43:06 +0700

    Lab Shell Script

---
+++ lab_bash/q10.sh
@@ -0,0 +1,8 @@
+int main() {
+    for file in $(find . -type f -name "L*.c")
+do
+ #trimmedName=`echo $file|cut -d'/' -f2`
+ trimmedName=`echo $file | cut -d'/' -f2 | cut -d'.' -f1`
+ echo $file $trimmedName
+done 
+}
